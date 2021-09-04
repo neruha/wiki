b06e431e386690a0e48a21b094ca319771ee31ad
hato810424 <yukkuri.hatopoppo@Gmail.com>
2021-09-04 14:56:43 +0900

    運営追加こみっと～

--- src/.vuepress/components/community/team/members.js
+++ src/.vuepress/components/community/team/members.js
@@ -136,7 +136,15 @@ const members = [
       languages: ['jp'],
       minecraft_uuid: 'c2c5071f71574930918473d49e8417bb',
       work: {
-        role: ['広報'],
+        role: '広報',
+      }
+    },
+    {
+      name: 'reitan234',
+      languages: ['jp'],
+      minecraft_uuid: 'cb446eb6b06f4cc4a2b66a3aa815ff83',
+      work: {
+        role: '接客',
       }
     }
   ])
